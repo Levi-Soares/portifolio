a1bf5ae367d4cc69c88000889bd57157197879ec
Levi Soares <levi-castro@outlook.com>
2025-08-01 11:34:38 -0300

    py email ajustado

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 from flask import Flask, render_template, redirect, request, flash
 from flask_mail import Mail, Message 
-from config import email,senha
+from config import email, senha
 
 app = Flask(__name__)
 app.secret_key = 'Portifolio'
 
 mail_settings = {
-    "MAIL_SERVER":'smtp.gmail.com',
+    "MAIL_SERVER": 'smtp.gmail.com',
     "MAIL_PORT": 465,
     "MAIL_USE_TLS": False,
-    "MAIL_USE_TLS": True,
+    "MAIL_USE_SSL": True,
     "MAIL_USERNAME": email,
-    "MAIL_PASSWORD": mail_senha,
+    "MAIL_PASSWORD": senha,
 }
 
 app.config.update(mail_settings)
@@ -19,11 +19,9 @@ mail = Mail(app)
 
 class Contato:
     def __init__(self, nome, email, mensagem):
-        self.nome = nome,
-        self.email = email,
+        self.nome = nome
+        self.email = email
         self.mensagem = mensagem
-
-
 
 @app.route('/')
 def index():
@@ -32,22 +30,20 @@ def index():
 @app.route('/send', methods=['GET', 'POST'])
 def send():
     if request.method == 'POST':
-        formContato - Contato(
+        formContato = Contato(
             request.form["nome"], 
             request.form["email"],
             request.form["mensagem"]
-                            )
+        )
         msg = Message(
-            subject= f'{formContato.nome} te enviou uma mesagem no portifólio',
-            sender = app.config.get("MAIL_USERNAME")
+            subject= f'{formContato.nome} te enviou uma mensagem no portfólio',
+            sender = app.config.get("MAIL_USERNAME"),
             recipients = ['levi-castro@outlook.com', 'leviswepper@gmail.com', app.config.get("MAIL_USERNAME")],
             body = f'''
+{formContato.nome} com o e-mail {formContato.email}, te enviou a seguinte mensagem:
 
-            {formContato.nome} com o e-mail {formContato.email}, te enviou a seguinte memsagem:
-
-            {formContato.mensagem}
-            
-            '''
+{formContato.mensagem}
+'''
         )
         mail.send(msg)
         flash('Mensagem enviada com sucesso!')
